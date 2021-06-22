85fc8cac3d2bf70d4b0f5bfc5e8ca5c561ecb9f5
ajmnz <arnaujim@gmail.com>
2021-06-22 13:12:37 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-full">
+      <div className="center-xy">
+        <div className="border border-primary-200 p-100 rounded-8">
+          This is centered in dev, but not when compiled
+        </div>
+      </div>
+    </div>
+  );
+}
